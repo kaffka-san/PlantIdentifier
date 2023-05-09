cd3815344448c990e7981b178b30e6b4d4ee806f
kaffka <kaffka.anastasia@gmail.com>
2023-05-09 13:49:32 +0200

    Initial Commit

---
+++ PlantIdentifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlantIdentifier
+//
+//  Created by Anastasia Lenina on 09.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
